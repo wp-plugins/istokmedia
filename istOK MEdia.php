de937603996d7f06dd18692fa1fec8780988916d
jaroslawistok <jaroslawistok@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-28 14:40:15 +0000

    istOKMEdia
    git-svn-id: https://plugins.svn.wordpress.org/istokmedia/trunk@1233166 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ istOK MEdia.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: istOKMEdia
+Plugin Script: 
+Plugin URI: http://www.istok.de/
+Description: istOK MEdia Werbe Server
+Version: 1.0
+Author: istok
+Author URI: http://www.istok.de/
+Template by: istok
+
+
+=== RELEASE NOTES ===
+2013-01-26 - v1.0 - first version
+*/
+add_action ('admin_menu', 'istOKMEdia_admin_actions');
+function istOKMEdia_admin_actions () {
+add_options_page ('istOKMEdia', 'istOKMEdia', 'manage_options', ____FILE____, 'istOKMEdia_admin');
+}
+function istOKMEdia_admin ()
+{
+?>
+<div class="wrap">
+<h4>istOK MEdia</h4>
+
+
+
+<p style="text-align: center;"><a href="http://www.istok.de/wp-content/uploads/2015/08/clickbank-logo1-300x60.png"><img class=" size-medium wp-image-17997 aligncenter" src="http://www.istok.de/wp-content/uploads/2015/08/clickbank-logo1-300x60-300x60.png" alt="clickbank-logo1-300x60" width="300" height="60" /></a></p>
+Warum Cent Beträge auf die Klicks verdienen wenn man bei diesem Modell, direkt anteilmässig von de hohen Clickbank Ausschüttungen profitieren kann. Testen Sie es 2-3 Monate und sehen selbst. Sie müssen sich nicht um die Wasserzeichen versehenen und auffindbaren Banner kümmern, auch nicht um die Programm Aktualisierungen, Starts und Endungen. Über 500 Werbe Banner werden jetzt im System rotiert.<a href="http://www.istok.de/wp-content/uploads/2015/08/WordPress-Plugin1-300x117.jpg">
+</a>
+
+&nbsp;
+<p style="text-align: center;"><a href="http://www.istok.de/wp-content/uploads/2015/08/WordPress-Plugin1-300x117.jpg"><img class=" size-medium wp-image-17998 aligncenter" src="http://www.istok.de/wp-content/uploads/2015/08/WordPress-Plugin1-300x117-300x117.jpg" alt="WordPress-Plugin1-300x117" width="300" height="117" /></a></p>
+&nbsp;
+
+Sie werden automatisch über Einblendungen, Klicks und Verkäufe informiert.
+
+Bitte als Partner (Publisher) bei istOK MEdia <a href="http://hst-15913-001.pod1.us01.hst.inclickadserver.com/signup.php?user_type=pub" target="_blank">registrieren</a>.
+
+Danach Einlogen und unter: Integrations Tools den Code erstellen (Auch Mischvarianten möglich bei gleicher Bannergrösse - z.B. Grafik und Textgrafik).
+Momentan sind 468x60, 728x90 und 250x250 Grafik Banner und Text oder Text+Bild in Allen Formaten vorhanden.
+
+Den erstellten Code Bitte unter Design/Widgets z.B. Im Seitenmenü platzieren oder sonstwo auf der Seite. Um die Werbung mittig zu zentrieren setzen Sie &lt;center&gt; vor und &lt;/center&gt; nachdem Code ein.
+
+Um die Werbeblocker der Seiten Betrachter zu kontrollieren schauen Sie unter: <a href="http://antiblock.org/" target="_blank">http://antiblock.org/</a>
+<p style="text-align: center;"><a href="http://www.istok.de/wp-content/uploads/2015/08/plugin-folder-icon.png"><img class="  wp-image-17999 aligncenter" src="http://www.istok.de/wp-content/uploads/2015/08/plugin-folder-icon-300x300.png" alt="plugin-folder-icon" width="146" height="146" /></a></p>
+Auf die angegebene Paypal Email Adresse werden Verkäufe vergütet und zwar im 3 Phasen Modell:
+
+-Anlauf mit Ratio 60% zu 40%
+-danach 70% zu 30%
+-Schliesslich 80% zu 20%
+
+Immer zu Ihrem Günsten. Phasen werden im Mailing angekündigt.
+
+Testen Sie es, es lohnt sich :o)
+<p style="text-align: center;"><a href="http://www.istok.de/wp-content/uploads/2015/08/WordPressLupe1.jpg"><img class=" size-full wp-image-18000 aligncenter" src="http://www.istok.de/wp-content/uploads/2015/08/WordPressLupe1.jpg" alt="WordPressLupe1" width="128" height="128" /></a></p>
+
+
+
+
+</div>
+<?php
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,75 @@
+=== istOK MEdia Plugin ===
+Contributors: Istok
+Donate link: http://www.istok.de/plugin/
+Tags: clickbank, deutsch, german, Monetarisierung, Banner, Werbung, Geld Verdienen, Digibux
+Requires at least: 3.0.1
+Tested up to: 4.2.4
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+istOK MEdia Bannerwerbung für Deutsches ClickBank zum Geld Verdienen (wie früher Digibux)
+
+== Description ==
+
+Warum Cent Beträge auf die Klicks verdienen wenn man bei diesem Modell, direkt anteilmässig von den hohen Clickbank Ausschüttungen profitieren kann, Testen Sie es 2-3 Monate und sehen selbst. Sie müssen sich nicht um die Wasserzeichen versehenen und auffindbaren Banner kümmern, auch nicht um die Programm Aktualisierungen, Starts und Endungen. Über 500 Werbe Banner werden jetzt im System rotiert.
+
+Sie werden automatisch über Einblendungen, Klicks und Verkäufe informiert.
+
+Bitte als Partner (Publisher) bei istOK MEdia registrieren.
+
+Danach Einlogen und unter: Integrations Tools den Code erstellen (Auch Mischvarianten möglich bei gleicher Bannergrösse - z.B. Grafik und Textgrafik).
+Momentan sind 468x60, 728x90 und 250x250 Grafik Banner und Text oder Text+Bild in Allen Formaten vorhanden.
+
+Den erstellten Code Bitte unter Design/Widgets z.B. Im Seitenmenü platzieren oder sonstwo auf der Seite. Um die Werbung mittig zu zentrieren setzen Sie <center> vor und </center> nachdem Code ein.
+
+Um die Werbeblocker der Seiten Betrachter zu kontrollieren schauen Sie unter: http://antiblock.org/
+
+Auf die angegebene Paypal Email Adresse werden Verkäufe vergütet und zwar im 3 Phasen Modell:
+
+-Anlauf mit Ratio 60% zu 40%
+-danach 70% zu 30%
+-Schliesslich 80% zu 20%
+
+Immer zu Ihrem Günsten. Phasen werden im Mailing angekündigt.
+
+Testen Sie es, es lohnt sich :o)
+
+
+See also http://www.istok.de/
+
+== Frequently Asked Questions ==
+
+Warum gibt es nur 1 Kanal?
+
+Ursprünglich gab es thematische Kanäle jedoch, nach Überlegung, ist es besser wenn mal 1 Werbung deplatziert gesehen wird als doch Vieles ungesehen bleiben würde. So z.B. kann sich auch eine Hausfrau für's Geldverdienen interessieren oder ein Webmaster sich um seine Gesundheit oder Liebes Probleme kümmern.
+
+Ist die Werbung Conten Sensitiv?
+
+Ja die Seiten werden kurz gescannt vor der Werbeinblendung, um nach Werbung, entsprechend vom Inhalt zu suchen.
+
+
+See http://www.istok.de/
+
+== Installation ==
+
+1. Bitte Ordner `GermanClickBank` uploaden an `/wp-content/plugins/`.
+2. Aktivieren unter 'Plugins' menu in WordPress.
+
+== Screenshots ==
+
+1. Nachdem Code platziert ist erscheint die Werbung an dieser Stelle.
+
+http://www.istok.de/
+
+== Changelog ==
+
+= 1.0 =
+* Erste Version
+
+== Upgrade Notice ==
+
+= 1.0 =
+Bitte kommende Updates beachten
+
